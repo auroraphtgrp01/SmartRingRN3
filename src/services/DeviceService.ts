bad02f00e5c92fb5685b8d1ec2a1833b20a0c299
auroraphtgrp <sliverknight4869@gmail.com>
2025-05-02 13:45:36 +0700

    sd

--- src/services/DeviceService.ts
+++ src/services/DeviceService.ts
@@ -4,9 +4,18 @@ const GET_DEVICE_INFO_DATA_TYPE = 512
 const GET_DEVICE_INFO_DATA = new Uint8Array([71, 67])
 
 export const handleGetDeviceInfo = () => {
-    ycbtClientImpl.sendSingleData2Device(GET_DEVICE_INFO_DATA_TYPE, GET_DEVICE_INFO_DATA, 2, {
-            onDataResponse: (status, data) => {
-              console.log('Thông tin thiết bị:', data);
+    const sendDeviceInfoRequest = () => {
+        ycbtClientImpl.sendSingleData2Device(GET_DEVICE_INFO_DATA_TYPE, GET_DEVICE_INFO_DATA, 2, {
+            onDataResponse: (status, data: any) => {
+                console.log('Thông tin thiết bị:', data);
+                
+                if (data && data.length === 7) {
+                    console.log('Độ dài dữ liệu = 7, gọi lại yêu cầu...');
+                    sendDeviceInfoRequest();
+                }
             }
-          });
+        });
+    };
+    
+    sendDeviceInfoRequest();
 }
